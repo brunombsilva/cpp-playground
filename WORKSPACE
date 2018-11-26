e9b3e86465dd9291d045cf99c8d0f10e8b9b961d
Bruno Silva <b.silva@live.com>
2018-11-26 09:49:00 +0000

    Add glog for logging

--- WORKSPACE
+++ WORKSPACE
@@ -6,3 +6,22 @@ http_archive(
     sha256 = "927827c183d01734cc5cfef85e0ff3f5a92ffe6188e0d18e909c5efebf28a0c7",
     strip_prefix = "googletest-release-1.8.1",
 )
+
+load("@bazel_tools//tools/build_defs/repo:git.bzl", "git_repository")
+
+git_repository(
+    name = "com_github_google_glog",
+    remote = "https://github.com/google/glog.git",
+    commit = "781096619d3dd368cfebd33889e417a168493ce7",
+    # TODO: sha256:
+)
+
+http_archive(
+    name = "com_github_gflags_gflags",
+    strip_prefix = "gflags-2.2.2",
+    urls = [
+        "https://mirror.bazel.build/github.com/gflags/gflags/archive/v2.2.2.tar.gz",
+        "https://github.com/gflags/gflags/archive/v2.2.2.tar.gz",
+    ],
+    sha256 = "34af2f15cf7367513b352bdcd2493ab14ce43692d2dcd9dfc499492966c64dcf"
+)
--- hello-world/main.cc
+++ hello-world/main.cc
@@ -1,7 +1,41 @@
 #include <iostream>
+#include <gflags/gflags.h>
+#include <glog/logging.h>
+#include <glog/stl_logging.h>
 
-int main(int argc, char const *argv[])
+DEFINE_bool(big_menu, true, "Include 'advanced' options in the menu listing");
+DEFINE_string(languages, "english,french,german",
+                "comma-separated list of languages to offer in the 'lang' menu");
+
+using namespace std;
+
+// https://gflags.github.io/gflags/
+
+//bazel run hello-world -- --logtostderr --nobig_menu --languages=a,b,c
+int main(int argc, char *argv[])
 {
-    std::cout << "Hello, World!" << std::endl;
+    // FLAGS_log_dir = "/Users/brunombsilva/Playground/cpp-playground/logs";
+    // FLAGS_logtostderr = 1;
+
+    // Optional: parse command line flags
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+    // Initialize Google's logging library.
+    google::InitGoogleLogging(argv[0]);
+
+    LOG(INFO) << "Hello, world!";
+
+    LOG(ERROR) << "Big Menu?: " << FLAGS_big_menu;
+    LOG(WARNING) << "Languages: " << FLAGS_languages;
+
+    // glog/stl_logging.h allows logging STL containers.
+    std::vector<int> x;
+    x.push_back(1);
+    x.push_back(2);
+    x.push_back(3);
+    LOG(INFO) << "ABC, it's easy as " << x;
+
+    std::cout << "Hello World" << std::endl;
+
     return 0;
 }
